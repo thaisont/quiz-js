0dbb5abb241ce06b110b3b5c95993f0bdae352f2
Thai Truong <sontruong@hotmail.co.uk>
2020-12-22 00:04:27 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const correctAnswers = ["A", "A", "A", "B"];
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let score = 0;
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
+
+  // Check answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25;
+    }
+  });
+
+  // display result
+  scrollTo(0, 0);
+
+  result.classList.remove("d-none");
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector("span").textContent = `${output}%`;
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
+});
+
+// Window object
+
+// console.log("hello");
+// window.console.log("hello");
+
+// console.log(document.querySelector("form"));
+
+// alert("hello");
+// window.alert("hello");
+
+// setTimeout(() => {
+//   alert("hello");
+// }, 3000);
+
+// let i = 0;
+// const timer = setInterval(() => {
+//   console.log("hello");
+//   i++;
+//   if (i === 5) {
+//     clearInterval(timer);
+//   }
+// }, 1000);
